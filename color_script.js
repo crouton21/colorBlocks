e9a1a24d2484ce1eb91e1fc84e6f53e001b39d4c
Christiana Routon <christianarouton@Christianas-MacBook-Pro.local>
2018-01-31 17:03:44 -0600

    base project complete

---
+++ color_script.js
@@ -0,0 +1,54 @@
+console.log("JS");
+
+$(document).ready(onReady);
+
+var redCount = 0;
+var yellowCount = 0;
+var blueCount = 0;
+var greenCount = 0;
+
+function onReady(){
+  $('.box').hide();
+  $('.redCount').on('click', addRedBox);
+  $('.yellowCount').on('click', addYellowBox);
+  $('.blueCount').on('click', addBlueBox);
+  $('.greenCount').on('click', addGreenBox);
+  $('.box').on('click', clickBox);
+}
+
+function addRedBox (){
+  console.log('in addRedBox');
+  redCount += 1;
+  $('.redNumber').empty();
+  $('.redNumber').append('Red Count: '+redCount);
+  $('#allBoxes').append('<div id="redBox" class="box"></div>');
+}
+
+function addYellowBox (){
+  console.log('in addYellowBox');
+  yellowCount += 1;
+  $('.yellowNumber').empty();
+  $('.yellowNumber').append('Yellow Count: '+yellowCount);
+  $('#allBoxes').append('<div id="yellowBox" class="box"></div>');
+}
+
+function addBlueBox (){
+  console.log('in addBlueBox');
+  blueCount += 1;
+  $('.blueNumber').empty();
+  $('.blueNumber').append('Blue Count: '+blueCount);
+  $('#allBoxes').append('<div id="blueBox" class="box"></div>');
+}
+
+function addGreenBox (){
+  console.log('in addGreenBox');
+  greenCount += 1;
+  $('.greenNumber').empty();
+  $('.greenNumber').append('Green Count: '+greenCount);
+  $('#allBoxes').append('<div id="greenBox" class="box"></div>');
+}
+
+function clickBox (e) {
+  console.log('in clickBox');
+  $(this).hide();
+}
